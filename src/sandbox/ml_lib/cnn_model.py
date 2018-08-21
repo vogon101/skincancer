67f67b6acac422e416ff62fa9ccd3d8fc5bc3ef0
Freddie Poser <freddie.poser@gmail.com>
2018-08-21 16:32:23 +0100

    Working towards presentation

--- src/sandbox/ml_lib/cnn_model.py
+++ src/sandbox/ml_lib/cnn_model.py
@@ -3,6 +3,7 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
 from ml_lib.dlmodel import DLModel
+from keras import optimizers
 
 
 class CNN(DLModel):
@@ -14,25 +15,25 @@ class CNN(DLModel):
         self.model = Sequential()
 
         # Input Shape (128,128,3)
-        self.model.add(Conv2D(filters=64, kernel_size=(3, 3), strides=(3, 3), input_shape=self.input_shape))
+        self.model.add(Conv2D(filters=64, kernel_size=(3, 3), strides=(1, 1), input_shape=self.input_shape))
         self.model.add(Activation('relu'))
 
         self.model.add(MaxPooling2D(pool_size=(2, 2)))
 
         # self.model.add(Dropout(0.5))
 
-        self.model.add(Conv2D(filters=128, kernel_size=(3, 3), strides=(3, 3)))
+        self.model.add(Conv2D(filters=64, kernel_size=(3, 3), strides=(1, 1)))
         self.model.add(Activation('relu'))
         # Shape: 61x61x32
 
         self.model.add(MaxPooling2D(pool_size=self.pool_size))
         # Shape: 30x30x32
 
-        #self.model.add(Conv2D(self.nb_filters * 2, self.kernel_size))
-        #self.model.add(Activation('relu'))
+        self.model.add(Conv2D(128, self.kernel_size))
+        self.model.add(Activation('relu'))
         # Shape: 28x28x64
 
-        #self.model.add(MaxPooling2D(pool_size=self.pool_size))
+        self.model.add(MaxPooling2D(pool_size=self.pool_size))
         # Shape: 14x14x64
 
         self.model.add(Flatten())
@@ -40,19 +41,17 @@ class CNN(DLModel):
         # Shape: 12544
 
         # now start a typical neural network
-        self.model.add(Dense(64))
-        self.model.add(Activation('relu'))
+        self.model.add(Dense(64, activation="relu"))
         # Shape: 64
 
         self.model.add(Dropout(0.2))
 
         print('Using 2 Classes')
-        self.model.add(Dense(1))
-        self.model.add(Activation('sigmoid'))
+        self.model.add(Dense(1, activation="sigmoid"))
         loss = 'binary_crossentropy'
-        self.model.compile(loss=loss,
-                           optimizer='adam',
-                           metrics=['accuracy'])
+        self.model.compile(loss='binary_crossentropy',
+                          optimizer=optimizers.SGD(lr=1e-4, momentum=0.90),
+                          metrics=['accuracy'])
 
 
 if __name__ == '__main__':
